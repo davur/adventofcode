5e3ac39c2ce4cdda23122e1ec57b823d321dc5db
davur <davur@davur.net>
2022-12-07 21:31:33 +1100

    Updated Day 7, 2022

--- year2022/day7.py
+++ year2022/day7.py
@@ -2,6 +2,7 @@ debug = False
 
 from .utils import *
 from collections import defaultdict
+from pprint import pprint
 
 
 def ints(nums):
@@ -9,16 +10,18 @@ def ints(nums):
 
 
 
-def fullpath(arr, extra = ''):
-    if extra:
-        newarr = arr + [extra]
-    else:
-        newarr = arr
 
-    if len(newarr) == 1:
-        return '/'
+
+
+def directory_stack_to_path(cur):
+    #cur = ['a', 'e', 'd']
+    #path = '/a/e/d/'
+
+    if len(cur) == 0:
+        return "/"
     else:
-        return '/'.join(newarr)
+        return "/%s/" % ("/".join(cur))
+
 
 def solution(file):
 
@@ -26,69 +29,80 @@ def solution(file):
     result2 = 0
 
     sections = read(file)
-
-    dirs = {}
-
-    dirsizes = defaultdict(int)
-
+    rows = sections[0]
+    
     cur = []
+    cur_path = None
+    dirsizes = defaultdict(int)
+    # = {
+    #   '/': 23352670, 
+    #   '/a/': 94269, 
+    #   '/a/e/': 584, 
+    #   '/d/': 24933642,
+    # }
 
-    for section in sections:
-        rows = section
+    for line in rows:
+        if line[0] == '$':
+            commands = line.split()
+            # $ cd /
+            # $ cd blarg
+            # $ ls
 
-        for line in rows:
+            if commands[1] == 'cd':
+                if commands[2] == '..':
+                    cur.pop()
+                elif commands[2] == '/':
+                    cur = []
+                else: # $ cd tmp
+                    cur.append(commands[2])
+                cur_path = directory_stack_to_path(cur)
 
-            if line[0] == '$':
-                command = line[2:].split()
-
-                if command[0] == 'cd':
-                    if command[1] == '/':
-                        cur = ['']
-                        dirsizes[fullpath(cur)] += 0
-                    elif command[1] == '..':
-                        cur.pop()
-                    else:
-                        cur.append(command[1])
-                        dirsizes[fullpath(cur)] += 0
-                # ignore ls
+            elif commands[1] == 'ls':
+                path = directory_stack_to_path(cur)
+                dirsizes[path] = 0
+                
+        else:
+            first, second = line.split()
+            if first == 'dir':
+                continue
             else:
-                size, path = line.split()
+                dirsizes[cur_path] += int(first)
 
-                if size == 'dir':
-                    dirsizes[fullpath(cur, path)] += 0
-                else:
-                    dirsizes[fullpath(cur)] += int(size)
+    dirsizes_list = [(path, dirsizes[path]) for path in dirsizes]
+    dirsizes_list.sort()
 
-        dirs = dirsizes.keys()
-        print(dirs)
+    total_dirsizes = []
+    
+    for i in range(len(dirsizes_list)):
+        path, dirsize = dirsizes_list[i]
+        total_dirsize = dirsize
+        j = i + 1
+        while j < len(dirsizes_list) and dirsizes_list[j][0].startswith(path):
+            total_dirsize += dirsizes_list[j][1]
+            j += 1
 
-        dir_totals = []
-        for dir in dirs:
-            dir_total = sum([dirsizes[p] for p in dirsizes if p[:len(dir)] == dir])
-            dir_totals.append((dir_total, dir))
-
-        # Part 1
-        result1 = 0
-        for s, _ in dir_totals:
-            if s <= 100000:
-                result1 += s
-
-        dir_totals.sort()
-        total = dir_totals[-1][0]
-        to_remove = 30000000-(70000000-total)
+        total_dirsizes.append((total_dirsize, path))
 
 
+    total_dirsizes.sort()
 
-        # Part1
-        for i in range(len(dir_totals)):
-            s, p = dir_totals[i]
-            if s >= to_remove: 
-                result2 = s
-                break
+    # Part 1:
+    result1 = 0
+    for i in range(len(total_dirsizes)):
+        dirsize = total_dirsizes[i][0]
+        if dirsize < 100000:
+            result1 += dirsize
 
-# tried 42536714
-# tried 23754
-# tried 2940614
+    total_size = total_dirsizes[-1][0]
+    disk_size = 70000000
+    size_required = 30000000
+    to_delete = size_required - (disk_size - total_size)
+
+    for i in range(len(total_dirsizes)):
+        dirsize = total_dirsizes[i][0]
+        if dirsize > to_delete:
+            result2 = dirsize
+            break
 
     return (result1, result2)
 
