815ce8e5cfa14b686c920a08264d4f235533feb7
davur <davur@davur.net>
2023-12-05 22:50:14 +1100

    2023 Day 5 Part 2 (Naive solves sample only)

--- year2023/day5.py
+++ year2023/day5.py
@@ -12,17 +12,28 @@ def solution(sections):
 
     result1 = result2 = 0
 
+    # Part 1
     seeds = [int(s) for s in sections[0][0].split(':')[1].split() if s.isdigit()]
     seeds.sort()
-    dprint(f'{seeds}')
+    dprint(f'{seeds=}')
 
-    lines = sections.pop(0)
-    # dprint(lines)
+    # Part 2
+    seeds2 = []
+    seed_numbers = [int(s) for s in sections[0][0].split(':')[1].split() if s.isdigit()]
+    while len(seed_numbers):
+        seeds_start = seed_numbers.pop(0)
+        seeds_len = seed_numbers.pop(0)
+
+        for i in range(seeds_len):
+            seeds2.append(seeds_start + i)
+    dprint(f'{seeds2=}')
+    print(len(seeds2))
 
     maps = []
 
-    for lines in sections:
-        dprint(lines[0])
+    for sec in range(1, len(sections)):
+        lines = sections[sec]
+        print(lines[0])
 
         mappings = []
         for i in range(1, len(lines)):
@@ -35,27 +46,41 @@ def solution(sections):
             # dprint(f'{r=}')
             mappings.append(r)
 
-        dprint(f'{seeds=}')
+        # dprint(f'{seeds=}')
         mappings.sort()
-        dprint(f'{mappings=}')
+        # dprint(f'{mappings=}')
 
+        # Part 1
         for i in range(len(seeds)):
             seed = seeds[i]
-            dprint(f'{seed=}')
+            # dprint(f'{seed=}')
 
             for mapping in mappings:
-                dprint(f'{mapping=}')
-                if mapping[0] <= seed <= mapping[1]:
+                # dprint(f'{mapping=}')
+                if mapping[0] <= seed < mapping[1]:
                     seed += mapping[2]
                     seeds[i] = seed
                     break
 
+        # Part 2
+        # dprint(f'{seeds2=}')
+        for i in range(len(seeds2)):
+            seed = seeds2[i]
 
-        dprint(f'{seeds=}')
+            for mapping in mappings:
+                if mapping[0] <= seed < mapping[1]:
+                    seed += mapping[2]
+                    seeds2[i] = seed
+                    break
+
+        # seeds2.sort()
+
+
+        dprint(f'{mappings=}')
+        dprint(f'{seeds2=}')
 
 
     result1 = min(seeds)
-
-
+    result2 = min(seeds2)
 
     return (result1, result2)
